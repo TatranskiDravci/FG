08aae6ddc3485bcb8cfce1c78ba6815cc2be6fe8
LukasDrsman <lukaskodr@gmail.com>
2022-09-07 16:11:50 +0200

    Add touch event trigger

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const MAX_IDX = 4;
 const MIN_IDX = 1;
-const TIMEOUT = 5000;
+const TIMEOUT = 3000;
 
 function promiseFactory(ms, i) {
-    let promise = new Promise(resolve => setTimeout(resolve, ms));
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
+        addEventListener("touchstart", reject);
+    });
+
+    promise.catch(() => {
+        addEventListener("touchend", () => promiseFactory(5000, i));
+    });
 
     i++;
     if (i > MAX_IDX) {
         i = MIN_IDX;
     }
 
-    promise.then(() => {
+    promise
+    .then(() => {
         document.getElementById("slide" + i).scrollIntoView({ behavior: 'smooth', block: 'center' });
-    }).then(() => promiseFactory(ms, i))
+    })
+    .then(() => promiseFactory(ms, i));
+
     return
 }
 
