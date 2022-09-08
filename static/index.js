b5a64099dd312cd12f39eb0952c18be5d699636c
LukasDrsman <lukaskodr@gmail.com>
2022-09-08 14:15:51 +0200

    Resolve resurrection to the nearest slide to viewport

--- static/index.js
+++ static/index.js
@@ -2,9 +2,20 @@ console.log("===== SOURCED =====");
 
 const MAX_IDX = 4;
 const MIN_IDX = 1;
-const TIMEOUT = 3000;
+const TIMEOUT = 5000;
 const TIMEOUT_LONG = 10000 - TIMEOUT;
 let dead = 0;
+
+
+function getNearestSlide() {
+    return (
+        ( 0                   <= window.pageYOffset && window.pageYOffset <   window.innerHeight) * 1 +
+        ( window.innerHeight  <= window.pageYOffset && window.pageYOffset < 2*window.innerHeight) * 2 +
+        (2*window.innerHeight <= window.pageYOffset && window.pageYOffset < 3*window.innerHeight) * 3 +
+        (3*window.innerHeight <= window.pageYOffset && window.pageYOffset < 4*window.innerHeight) * 4
+    )
+}
+
 
 function promiseFactory(ms, i, parent) {
     console.log(i, parent);
@@ -39,18 +50,18 @@ function promiseFactory(ms, i, parent) {
     return
 }
 
-promiseFactory(TIMEOUT, MIN_IDX, "source-1");
+promiseFactory(TIMEOUT, getNearestSlide(), "source-" + getNearestSlide());
 
 addEventListener("touchend", () => {
     if (dead == 1) {
-        promiseFactory(TIMEOUT, MIN_IDX, "resurrected-1");
+        setTimeout(promiseFactory(TIMEOUT, getNearestSlide(), "resurrected-" + getNearestSlide()), TIMEOUT_LONG);
         dead = 0;
     }
 });
 
 addEventListener("focusout", () => {
     if (dead) {
-        promiseFactory(TIMEOUT, MIN_IDX, "resurrected-1");
+        setTimeout(promiseFactory(TIMEOUT, getNearestSlide(), "resurrected-" + getNearestSlide()), TIMEOUT_LONG);
         dead = 0;
     }
 });
@@ -65,21 +76,29 @@ async function populate() {
     const ul = document.getElementById("profesia_embed");
 
     obj.forEach(item => {
-        let li = document.createElement("li");
-        let h = document.createElement("h1");
-        let p_loc = document.createElement("p");
-        let p_pay = document.createElement("p");
+        let li = document.createElement("div");
+        let h = document.createElement("div");
+        let p_loc = document.createElement("div");
+        let p_pay = document.createElement("div");
 
         h.innerHTML = item.title;
         p_loc.innerHTML = item.loc;
         p_pay.innerHTML = item.pay;
+
+        h.classList.add("list_title");
+        h.classList.add("h2");
+        p_loc.classList.add("list_loc");
+        p_pay.classList.add("list_money");
+
+        li.classList.add("grey");
+        li.classList.add("embed_card")
 
         li.append(h);
         li.append(p_loc);
         li.append(p_pay);
 
         li.addEventListener("click", () => { 
-            fetch("/click").then(() => window.location.href = item.url);
+            fetch("/click").then(() => window.open(item.url));
         });
 
         ul.append(li);
